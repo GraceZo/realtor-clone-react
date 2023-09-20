ec27c9b16c33679aaa6b6675413a8b79a4b31427
chongyangzhong <chongyang741586437@gmail.com>
2023-09-20 20:34:56 +1000

    create sign in, sign up and forgot password pages and oauth component

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
-
-export default function ForgotPassword() {
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import OAuth from '../components/OAuth'
+export default function ForgetPassword() {
+  const [email, setEmail] = useState('')
+  function onChange(e) {
+    setEmail(e.target.value)
+  }
   return (
-    <div>ForgotPassword</div>
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
+      <div className="flex flex-col items-center flex-wrap px-6 py-12 mx-auto">
+        <form
+          action=""
+          className="w-full md:w-[67%] lg:w-[40%] mt-6 text-xl text-gray-700 border-gray-300 rounded transition ease-in-out"
+        >
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={onChange}
+            placeholder="Email address"
+            className="mb-6 w-full p-2 border border-gray-300 rounded"
+          />
+          <div className="relative mb-6"></div>
+          <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+            <p className="mb-6">
+              Don't have a account?
+              <Link
+                to="/sign-up"
+                className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+              >
+                Register
+              </Link>
+            </p>
+            <p>
+              <Link
+                to="/sign-in"
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
+              >
+                Sign in instead
+              </Link>
+            </p>
+          </div>
+          <button
+            type="submit"
+            className="w-full md:w-[40%] bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+          >
+            Send reset password
+          </button>
+          <div className="w-full flex items-center my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+            <p className="text-center font-semibold mx-4">OR</p>
+          </div>
+          <OAuth />
+        </form>
+      </div>
+    </section>
   )
 }
